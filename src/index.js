0cdd0ba7b640ffd30aaa88fbf76e221665eaba0e
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-08-23 13:50:21 +0200

    Update printNum

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ const isGreaterThanFive = require( './isGreaterThanFive' )
  * @param num {Number} The number we are checking
  */
 const printNum = ( num ) => {
-  console.log( `Is ${ num } greater than five?: ${ isGreaterThanFive( num ) }` )
+  console.log( `Is '${ num }' (${ typeof num }) greater than five?: ${ isGreaterThanFive( num ) }` )
 }
 
-printNum( 10 )
 printNum( 4 )
+printNum( 10 )
+printNum( null )
+printNum( undefined )
 printNum()
+printNum( '' )
+printNum( '100' )
+printNum( false )
